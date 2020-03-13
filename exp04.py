c6b36d6a86961942f3a6f7eb08a356ce650aca60
Thaistav <40217491+Thaistav@users.noreply.github.com>
2020-03-13 17:46:30 -0300

    Add files via upload

---
+++ exp04.py
@@ -0,0 +1,10 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+n1=float(input('Insira a primeira nota: '))
+n2=float(input('Insira a segunda nota: '))
+n3=float(input('insira a terceira nota: '))
+n4=float(input('insira a quarta nota: '))
+
+m=(n1+n2+n3+n4)/4
+
+print('A média é igual a: {:.2f}  '.format(m))
+         
---
+++ exp05.py
@@ -0,0 +1,4 @@
+#Faça um Programa que converta metros para centímetros.
+m=float(input('Digite a medidade em metros '))
+c=m*100
+print('O valor corresponde a {} centimentros'.format(c))
