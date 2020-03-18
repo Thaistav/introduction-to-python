02ebc7fd45e987fb7d387782ce54007a93821474
Thaistav <40217491+Thaistav@users.noreply.github.com>
2020-03-18 20:53:29 -0300

    Delete exp03.py

--- exp03.py
+++
@@ -1,5 +0,0 @@
-#Faça um Programa que peça dois números e imprima a soma.
-num1=int(input('Digite um número: '))
-num2=int(input('Digite outro número: '))
-soma=num1+num2
-print("A soma de {} + {} = {}" .format(num1, num2, soma))
