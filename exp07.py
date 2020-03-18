d482cc6fce7a1d3113d9807bef03a88ddce02834
Thaistav <40217491+Thaistav@users.noreply.github.com>
2020-03-18 20:55:25 -0300

    Delete exp07.py

--- exp07.py
+++
@@ -1,4 +0,0 @@
-#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
-l=float(input("Digite a medida do lado do quadrado: "))
-a=(l**2)*2
-print('A area do quadrado é' ,a)
